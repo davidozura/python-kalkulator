34ad822ba820033b63b04cf33b1348616fac5aa6
Ožura David <dozura@onko-i.si>
2022-02-26 20:01:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+numberone = int(input("Input first number: "))
+numbertwo = int(input("Input second number: "))
+
+if numberone == numbertwo:
+    print("Numbers are equal")
+elif numberone < numbertwo:
+    print("Second number", numbertwo, "is higher")
+else:
+    print("First number", numberone, "is higher")
+
+
+
